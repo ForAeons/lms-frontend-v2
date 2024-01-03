4471f3280bfd383f5e26276fcdc7d53544fa6693
ForAeons <w.xy020203@gmail.com>
2024-01-03 17:21:50 +0800

    fix object literal attributes

--- src/modules/auditlog/util/column.ts
+++ src/modules/auditlog/util/column.ts
@@ -1,41 +1,27 @@
 import { format } from "date-fns";
 import { ColumnDef } from "@tanstack/react-table";
 import { IntlWrapper } from "@/components/language-provider";
-import { TranslationKey } from "@/util";
-
-export const AuditLogColumns = [
-	{
-		accessorKey: "user_id",
-		header: "User ID",
-		intlID: "userID",
-	},
-	{
-		accessorKey: "user.username",
-		header: "Username",
-		intlID: "Username",
-	},
-	{
-		accessorKey: "action",
-		header: "Action",
-		intlID: "Action",
-	},
-	{
-		accessorKey: "date",
-		header: "Date",
-		intlID: "Date",
-		cell: ({ row }) => {
-			return format(row.getValue("date"), "P");
-		},
-	},
-] satisfies ColumnDef<AuditLog>[];
 
 export const getTranslatedColumns = (): ColumnDef<AuditLog>[] => {
-	return AuditLogColumns.map((column) => {
-		return {
-			...column,
-			header: (
-				IntlWrapper.translator[column.intlID as TranslationKey] as () => string
-			)(),
-		};
-	});
+	return [
+		{
+			accessorKey: "user_id",
+			header: IntlWrapper.translator.userID(),
+		},
+		{
+			accessorKey: "user.username",
+			header: IntlWrapper.translator.Username(),
+		},
+		{
+			accessorKey: "action",
+			header: IntlWrapper.translator.Action(),
+		},
+		{
+			accessorKey: "date",
+			header: IntlWrapper.translator.Date(),
+			cell: ({ row }) => {
+				return format(row.getValue("date"), "P");
+			},
+		},
+	];
 };
