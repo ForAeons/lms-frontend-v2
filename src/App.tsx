6842a7f4831e7bbe30a171050644fa01d64984e2
Wu Xiaoyun <w.xy020203@gmail.com>
2024-01-24 15:22:39 +0800

    feat: integrate csrf feature (#59)
    * feat: integrate csrf feature
    
    * chore: remove vercel config and packages

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,6 @@ import React from "react";
 import { Provider } from "react-redux";
 import { RouterProvider } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { Analytics } from "@vercel/analytics/react";
-import { SpeedInsights } from "@vercel/speed-insights/react";
 import { ThemeProvider } from "@/components/theme-provider";
 import { LanguageProvider } from "@/components/language-provider";
 import { Toaster } from "@/components/ui/sonner";
@@ -12,31 +10,29 @@ import { Router } from "@/router";
 import { store } from "@/store";
 
 const queryClient = new QueryClient({
-	defaultOptions: {
-		queries: { staleTime: 1000 * 60 * 5 },
-	},
+  defaultOptions: {
+    queries: { staleTime: 1000 * 60 * 5 },
+  },
 });
 
 export const App: React.FC = () => {
-	return (
-		<ThemeProvider
-			defaultTheme="light"
-			defaultColor="slate"
-			storageKey="vite-ui-theme"
-		>
-			<LanguageProvider defaultLocale="en" storageKey="vite-language">
-				<QueryClientProvider client={queryClient}>
-					<Provider store={store}>
-						<RouterProvider router={Router} />
-						<Toaster closeButton />
-						<TanStackQueryDevtools initialIsOpen={false} />
-						<Analytics />
-						<SpeedInsights />
-					</Provider>
-				</QueryClientProvider>
-			</LanguageProvider>
-		</ThemeProvider>
-	);
+  return (
+    <ThemeProvider
+      defaultTheme="light"
+      defaultColor="slate"
+      storageKey="vite-ui-theme"
+    >
+      <LanguageProvider defaultLocale="en" storageKey="vite-language">
+        <QueryClientProvider client={queryClient}>
+          <Provider store={store}>
+            <RouterProvider router={Router} />
+            <Toaster closeButton />
+            <TanStackQueryDevtools initialIsOpen={false} />
+          </Provider>
+        </QueryClientProvider>
+      </LanguageProvider>
+    </ThemeProvider>
+  );
 };
 
 export default App;
--- src/modules/common/components/on-mount-logic.tsx
+++ src/modules/common/components/on-mount-logic.tsx
@@ -9,75 +9,83 @@ import * as Api from "@/api";
 import { ErrorPage, LoadingPage } from "..";
 
 export const OnMountLogic: React.FC = () => {
-	const dispatch = useAppDispatch();
+  const dispatch = useAppDispatch();
 
-	const { status: backendFetchStatus } = useQuery({
-		queryKey: ["health"],
-		staleTime: Number.POSITIVE_INFINITY,
-		refetchInterval: 1000 * 30, // 30 seconds
-		queryFn: ({ signal }) => Api.baseApi.GetHealth(signal),
-	});
+  const { status: backendFetchStatus, data: csrf } = useQuery({
+    queryKey: ["health"],
+    staleTime: Number.POSITIVE_INFINITY,
+    refetchInterval: 1000 * 30, // 30 seconds
+    queryFn: ({ signal }) => Api.baseApi.GetHealth(signal),
+  });
 
-	const { status: userFetchStatus, data } = useQuery({
-		enabled: backendFetchStatus === "success",
-		queryKey: ["current_user"],
-		staleTime: Number.POSITIVE_INFINITY,
-		retry: false,
-		queryFn: ({ signal }) => Api.userApi.GetCurrentUser(signal),
-	});
+  React.useEffect(() => {
+    if (backendFetchStatus === "success") {
+      dispatch(appSlice.actions.setCsrfToken(csrf.data));
+    }
+  }, [dispatch, backendFetchStatus, csrf]);
 
-	React.useEffect(() => {
-		if (userFetchStatus !== "success" || !data) {
-			dispatch(appSlice.actions.setSignout());
-		} else if (data.data.is_logged_in) {
-			dispatch(appSlice.actions.setSignin(data.data));
-		}
-	}, [dispatch, userFetchStatus, data]);
+  const { status: userFetchStatus, data: userData } = useQuery({
+    enabled: backendFetchStatus === "success",
+    queryKey: ["current_user"],
+    staleTime: Number.POSITIVE_INFINITY,
+    retry: false,
+    queryFn: ({ signal }) => Api.userApi.GetCurrentUser(signal),
+  });
 
-	// prefetch all relevant data
-	const cq = useCollectionQuery();
-	const queryClient = useQueryClient();
-	// Home page books
-	queryClient.prefetchQuery({
-		queryKey: [Api.BookRoutes.BASE, cq],
-		queryFn: ({ signal }) => Api.bookApi.ListBook(cq, signal),
-	});
-	queryClient.prefetchQuery({
-		queryKey: [Api.BookRoutes.BASE, Api.BookRoutes.POPULAR.ROUTE],
-		queryFn: ({ signal }) => Api.bookApi.ListPopularBooks(signal),
-	});
+  React.useEffect(() => {
+    if (userFetchStatus !== "success" || !userData) {
+      dispatch(appSlice.actions.setSignout());
+    } else if (userData.data.is_logged_in) {
+      dispatch(appSlice.actions.setSignin(userData.data));
+    }
+  }, [dispatch, userFetchStatus, userData]);
 
-	// prefetch all user related data, if user is logged in
-	const user = useAppSelector((state) => state.app.user);
-	if (user?.id) {
-		const bmCq = newUserCollectionQuery(user?.id);
-		queryClient.prefetchQuery({
-			queryKey: [Api.BookmarkRoutes.BASE, bmCq],
-			queryFn: ({ signal }) => Api.bookmarkApi.ListBookmarks(bmCq, signal),
-		});
-		const loanCq = newUserCollectionQuery(user?.id, "borrowed");
-		queryClient.prefetchQuery({
-			queryKey: [Api.LoanRoutes.BASE, loanCq],
-			queryFn: ({ signal }) => Api.loanApi.ListLoan(loanCq, signal),
-		});
-		const resCq = newUserCollectionQuery(user?.id, "pending");
-		queryClient.prefetchQuery({
-			queryKey: [Api.ResRoutes.BASE, resCq],
-			queryFn: ({ signal }) => Api.reservationApi.ListRes(resCq, signal),
-		});
-		const fineCq = newUserCollectionQuery(user?.id, "outstanding");
-		queryClient.prefetchQuery({
-			queryKey: [Api.FineRoutes.BASE, fineCq],
-			queryFn: ({ signal }) => Api.fineApi.ListFine(fineCq, signal),
-		});
-	}
+  // prefetch all relevant data
+  const cq = useCollectionQuery();
+  const queryClient = useQueryClient();
+  if (backendFetchStatus === "success") {
+    // Home page books
+    queryClient.prefetchQuery({
+      queryKey: [Api.BookRoutes.BASE, cq],
+      queryFn: ({ signal }) => Api.bookApi.ListBook(cq, signal),
+    });
+    queryClient.prefetchQuery({
+      queryKey: [Api.BookRoutes.BASE, Api.BookRoutes.POPULAR.ROUTE],
+      queryFn: ({ signal }) => Api.bookApi.ListPopularBooks(signal),
+    });
+  }
 
-	const translate = useTranslations();
+  // prefetch all user related data, if user is logged in
+  const user = useAppSelector((state) => state.app.user);
+  if (user?.id) {
+    const bmCq = newUserCollectionQuery(user?.id);
+    queryClient.prefetchQuery({
+      queryKey: [Api.BookmarkRoutes.BASE, bmCq],
+      queryFn: ({ signal }) => Api.bookmarkApi.ListBookmarks(bmCq, signal),
+    });
+    const loanCq = newUserCollectionQuery(user?.id, "borrowed");
+    queryClient.prefetchQuery({
+      queryKey: [Api.LoanRoutes.BASE, loanCq],
+      queryFn: ({ signal }) => Api.loanApi.ListLoan(loanCq, signal),
+    });
+    const resCq = newUserCollectionQuery(user?.id, "pending");
+    queryClient.prefetchQuery({
+      queryKey: [Api.ResRoutes.BASE, resCq],
+      queryFn: ({ signal }) => Api.reservationApi.ListRes(resCq, signal),
+    });
+    const fineCq = newUserCollectionQuery(user?.id, "outstanding");
+    queryClient.prefetchQuery({
+      queryKey: [Api.FineRoutes.BASE, fineCq],
+      queryFn: ({ signal }) => Api.fineApi.ListFine(fineCq, signal),
+    });
+  }
 
-	if (backendFetchStatus === "pending") return <LoadingPage />;
+  const translate = useTranslations();
 
-	if (backendFetchStatus === "error")
-		return <ErrorPage errorMsg={translate.serverDownDesc()} />;
+  if (backendFetchStatus === "pending") return <LoadingPage />;
 
-	return <Outlet />;
+  if (backendFetchStatus === "error")
+    return <ErrorPage errorMsg={translate.serverDownDesc()} />;
+
+  return <Outlet />;
 };
